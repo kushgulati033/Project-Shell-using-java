cab04a98dacc77f2459983b6dd37cc2ea76348ee
kushgulati033 <kushgulati150@gmail.com>
2024-12-29 22:17:32 +0530

    path2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -54,7 +54,18 @@ public class Main {
                     }
                     break;
                 default:
-                    System.out.println(input + ": command not found");
+                    if (!parameter.equals("")) {
+                        String path = getPath(command);
+                        if (path != null) {
+                            String[] fullPath = new String[] {command, parameter};
+                            Process process = Runtime.getRuntime().exec(fullPath);
+                            process.getInputStream().transferTo(System.out);
+                        } else {
+                            System.out.println(command + ": command not found");
+                        }
+                    } else {
+                        System.out.println(input + ": command not found");
+                    }
             }
         }
     }
