ca13f61d260da394a2ec9d2e79a288487e71ee51
kushgulati033 <kushgulati150@gmail.com>
2024-12-29 19:26:57 +0530

    type builtin

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,6 +11,12 @@ public class Main {
             }
             if (input.startsWith("echo")) {
                 System.out.println(input.substring(5));
+            } else if (input.startsWith("type")) {
+                if(input.substring(5).equals("type") || input.substring(5).equals("echo") || input.substring(5).equals("exit")){
+                System.out.println(input.substring(5)+" is a shell builtin");}
+                else{
+                    System.out.println(input + ": command not found");
+                }
             } else {
                 System.out.println(input + ": command not found");
             }
